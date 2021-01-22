10af8784d59f10e2469a6f9d0624ac6af1e73fa3
Tykok <56304246+Tykok@users.noreply.github.com>
2021-01-22 17:46:22 +0100

    Documents
    Différents documents accompagnant le projet Java (UML, Fichier PDF du projet, ...)

--- src/main/java/fr/miage/toulouse/ProjetL3/Main.java
+++
@@ -1,9 +0,0 @@
-package fr.miage.toulouse.ProjetL3;
-
-public class Main {
-
-	public static void main(String[] args) {
-		App.main(args);
-	}
-
-}
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module fr.miage.toulouse.ProjetL3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens fr.miage.toulouse.ProjetL3 to javafx.fxml;
-    exports fr.miage.toulouse.ProjetL3;
-}
