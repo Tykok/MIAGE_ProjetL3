2f15e9e64e55cdf86668318ea2683ccb891bccba
Tykok <56304246+Tykok@users.noreply.github.com>
2021-02-05 01:31:48 +0100

    Contrôleurs
    Ajout des différents contrôleurs, test de démarrage de l'application
    Quelques errurs perçu sur certaines vues (à chercher)
    Différentes classes métiers à modifier aussi suite aux modifications apportés suite à l'analyse du fichier détaillant le projet

--- src/main/java/fr/miage/toulouse/ProjetL3/controleur/ConnexionController.java
+++ src/main/java/fr/miage/toulouse/ProjetL3/controleur/ConnexionController.java
@@ -1,21 +1,31 @@
 package fr.miage.toulouse.ProjetL3.controleur;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
-import fr.miage.toulouse.ProjetL3.*;
-public class ConnexionController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-	
-    @FXML
-    private void switchToAcceuil() throws IOException {
-    	// Fait appel à setRoot pour mettre à jour la fenêtre avec le .fxml nommé seondary
-        App.setRoot("Acceuil");
-    }
-    
-    @FXML
-    private void switchToError() throws IOException {
-    	// Fait appel à setRoot pour mettre à jour la fenêtre avec le .fxml nommé seondary
-        App.setRoot("primary");
-    }
-    
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import fr.miage.toulouse.ProjetL3.*;
+
+public class ConnexionController implements Initializable {
+
+	/**
+	 * Cette méthode réagit lors du clic sur le bouton "Valider" de la page de
+	 * connexion afin de vérifier si la personne qui ce connecte en as les droits,
+	 * et quels sont ces droits
+	 * 
+	 * @throws IOException
+	 */
+	@FXML
+	private void verifConnexion() throws IOException {
+		App.setRoot("ListeEtudants");// Fait appel à setRoot pour mettre à jour la fenêtre avec le .fxml donné
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
